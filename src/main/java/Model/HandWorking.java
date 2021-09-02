9b8ff20bfe32f56a46ea518455271f19d090f996
Albertymacedo <alberty.macedo@acad.ifma.edu.br>
2021-09-02 15:08:17 -0300

    new commit

--- src/main/java/Model/HandWorking.java
+++ src/main/java/Model/HandWorking.java
@@ -1,5 +1,9 @@
 package Model;
+import java.awt.event.*;
+import java.awt.Robot;
 
 public class HandWorking extends RepresentationHand implements IRepresentMovs {
+    Robot robot = new Robot();
+    int mask = InputEvent.BUTTON1_DOWN_MASK;
 
 }
--- src/main/java/Model/IReturnGetMovs.java
+++ src/main/java/Model/IReturnGetMovs.java
@@ -1,6 +1,6 @@
 package Model;
 
 public interface IReturnGetMovs {
-    return IReturnGetMovs
+
 
 }
